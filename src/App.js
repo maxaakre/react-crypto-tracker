fb7c9beea81cb873999a4ebba08701d2fbf5ac89
Max Aakre <maxaakre@gmail.com>
2021-10-28 21:09:10 +0200

    small change

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
         );
         setCoins(data);
-        setLoading("Here is your data");
+        setLoading("");
       } catch (e) {
         setError(e.messeage);
         setLoading("Something went wrong..");
